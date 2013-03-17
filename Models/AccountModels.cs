167a1f6126ce8fa65331b85aba536673418f1b5a
Tom Moseley <tjm09d@gmail.com>
2013-03-17 02:17:40 -0400

    Working on cleaning up login

--- Models/AccountModels.cs
+++ Models/AccountModels.cs
@@ -25,6 +25,11 @@ namespace Ludus.Models
         [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
         public int UserId { get; set; }
         public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string DisplayName { get; set; }
+        public string InstitutionUserId { get; set; }
+        public string EmailAddress { get; set; }
     }
 
     public class RegisterExternalLoginModel
