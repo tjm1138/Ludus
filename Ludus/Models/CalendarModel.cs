ad8f802d03af378314183a8161ebef9a9824e4d3
Tom Moseley <tjm09d@gmail.com>
2013-03-27 21:25:25 -0400

    Started Accordian

---
+++ Ludus/Models/CalendarModel.cs
@@ -0,0 +1,38 @@
+﻿namespace Ludus.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity;
+    using System.Globalization;
+    using System.Web.Security;
+    public class CalendarItem
+    {
+        public enum SourceEnum
+        {
+            Personal,
+            Course
+        };
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        public SourceEnum Source { get; set; }
+
+        [Display(Name = "Due")]
+        [Required]
+        public System.DateTime Due { get; set; }
+
+        [Display(Name = "Short Description")]
+        [Required]
+        public string ShortDescription { get; set; }
+
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+
+        [Display(Name = "Complete")]
+        [Required]
+        public bool Complete { get; set; }
+    }
+}
