dbbf715a0bc71e5199781aa4a281f4a2b9dba134
Shawn Williams <spw1185@gmail.com>
2013-03-31 03:23:42 -0500

    Manage Courses functionality

---
+++ Ludus/Controllers/CourseController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using Ludus.Filters;
+using Ludus.Models;
+using WebMatrix.WebData;
+
+namespace Ludus.Controllers
+{
+    [InitializeSimpleMembership]
+    public class CourseController : Controller
+    {
+        private DataServices.CourseService ds = new DataServices.CourseService();
+        private DataContext dc = new DataContext();
+
+        // GET: /Course/
+        public ActionResult Index()
+        {
+            ViewBag.Label = "Courses";
+            return View(ds.DisplayCourses());
+        }
+
+        // GET: /Course/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        // POST: /Course/Create
+
+        [HttpPost]
+        public ActionResult Create(Course Course)
+        {
+            if (ModelState.IsValid)
+            {
+                ds.Create(Course);
+                return RedirectToAction("Index");
+            }
+
+            return View(Course);
+        }
+
+        // GET: /Course/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Course Course = ds.Find(id);
+            if (Course == null)
+            {
+                return HttpNotFound();
+            }
+            return View(Course);
+        }
+
+        // POST: /Course/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Course Course)
+        {
+            if (ModelState.IsValid)
+            {
+                ds.Update(Course);
+                return RedirectToAction("Index");
+            }
+            return View(Course);
+        }
+
+        // GET: /Course/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Course Course = ds.Find(id);
+            if (Course == null)
+            {
+                return HttpNotFound();
+            }
+            return View(Course);
+        }
+
+        // POST: /Course/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ds.Remove(id);
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            ds.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Ludus/DataServices/CourseService.cs
@@ -0,0 +1,46 @@
+﻿namespace Ludus.DataServices
+{
+    using Ludus.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Linq;
+    using System.Web.Security;
+    using WebMatrix.WebData;
+    public class CourseService : IDisposable
+    {
+        private Ludus.Models.DataContext dc = new Ludus.Models.DataContext();
+        public ICollection<Course> DisplayCourses()
+        {
+            ICollection<Course> returnValue;
+            returnValue = (from pi in dc.Courses
+                           select pi).ToList();
+            return returnValue;
+        }
+        public Course Find(int id)
+        {
+            return dc.Courses.Find(id);
+        }
+        public void Remove(int id)
+        {
+            Course Course = Find(id);
+            dc.Courses.Remove(Course);
+            dc.SaveChanges();
+        }
+        public void Create(Course Course)
+        {
+            dc.Courses.Add(Course);
+            dc.SaveChanges();
+        }
+        public void Update(Course item)
+        {
+            dc.Entry(item).State = EntityState.Modified;
+            dc.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            dc.Dispose();
+        }
+    }
+}
