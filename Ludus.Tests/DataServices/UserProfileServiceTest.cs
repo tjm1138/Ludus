0e73bf8d52cdd369a499881ebf5d99599afe95e4
Kassy Coan <kassycoan@gmail.com>
2013-04-13 23:43:21 -0700

    Sets base of Profile testing. Testing not complete.

---
+++ Ludus.Tests/DataServices/UserProfileServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ludus.Tests.DataServices
+{
+    using Ludus.DataServices;
+    using Ludus.Filters;
+
+    [TestClass]
+    [InitializeSimpleMembership]
+
+    public class UserProfileServiceTest
+    {
+        [TestMethod]
+        public void GravatarTest()
+        {
+            // Arrange
+            UserProfileService service = new UserProfileService();
+
+        }
+    }
+}
