32d204eab1c79d98499bbf669d5bd0d0687b829a
Shawn Williams <spw1185@gmail.com>
2013-04-14 10:25:00 -0500

    Added comments to my Models

--- Ludus/Models/Section.cs
+++ Ludus/Models/Section.cs
@@ -1,9 +1,17 @@
+/*
+Section Model - All section-related items derive from this.
+Shawn Williams
+March 31, 2013
+*/
+
 namespace Ludus.Models
 {
     using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
+    
+    // SQL table Section
     [Table("Section")]
     public class Section
     {
@@ -15,31 +23,48 @@ namespace Ludus.Models
             this.Quizs = new HashSet<Quiz>();
         }
 
+        // Primary Key
         [Key]
         public int Id { get; set; }
 
+        // Session Id
         [Display(Name = "Session Id"), Required]
         public int SessionId { get; set; }
         
+        // Course Id
         [Display(Name = "Course Id"), Required]
         public int CourseId { get; set; }
 
+        // Section Number
         [Display(Name = "Section Number"), Required]
         public string Number { get; set; }
 
+        // Student Limit
         [Display(Name = "Student Limit"), Required]
         public int Limit { get; set; }
 
+        // Section Description
         [Display(Name = "Description"), Required]
         public string Description { get; set; }
-    
-        public virtual ICollection<Assignment> Assignments { get; set; }
-        public virtual ICollection<Badge> Badges { get; set; }
+        
+        // Foreign Key Course Id
         [ForeignKey("CourseId")]
         public virtual Course Course { get; set; }
-        public virtual ICollection<Enrollment> Enrollments { get; set; }
-        public virtual ICollection<Quiz> Quizs { get; set; }
+        
+        // Foreign Key Session Id
         [ForeignKey("SessionId")]
         public virtual Session Session { get; set; }
+
+        // extends IOCollection Assignments
+        public virtual ICollection<Assignment> Assignments { get; set; }
+
+        // extends IOCollection Badges
+        public virtual ICollection<Badge> Badges { get; set; }
+
+        // extends IOCollection Enrollments
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+
+        // extends IOCollection Quizs
+        public virtual ICollection<Quiz> Quizs { get; set; }
     }
 }
--- Ludus/Models/Student.cs
+++ Ludus/Models/Student.cs
@@ -1,9 +1,17 @@
+/*
+Student Model - All student-related items derive from this.
+Shawn Williams
+March 31, 2013
+*/
+
 namespace Ludus.Models
 {
     using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
+    
+    // SQL table Student
     [Table("Student")]
     public class Student
     {
@@ -13,22 +21,32 @@ namespace Ludus.Models
             this.StudentBadgeLinks = new HashSet<StudentBadgeLink>();
         }
 
+        // Primary Key
         [Key]
         public int Id { get; set; }
 
+        // User Id
         [Display(Name = "User Id")]
         [Required]
         public int UserId { get; set; }
 
+        // Session Id
         [Display(Name = "Session Id")]
         [Required]
         public int SessionId { get; set; }
 
-        public virtual ICollection<Enrollment> Enrollments { get; set; }
+        // Foreign Key Session Id
         [ForeignKey("SessionId")]
         public virtual Session Session { get; set; }
+        
+        // Foreign Key User Id
         [ForeignKey("UserId")]
         public virtual UserProfile UserProfile { get; set; }
+
+        // extends ICollection Enrollments
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+
+        // extends ICollection StudentBadgeLinks
         public virtual ICollection<StudentBadgeLink> StudentBadgeLinks { get; set; }
     }
 }
