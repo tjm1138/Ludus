00812595c47b27d04e9c9a1f07d09f1e46b1af7c
Kassy Coan <kassycoan@gmail.com>
2013-04-14 18:01:10 -0700

    Adds full update testing of profile fields.

--- Ludus.Tests/DataServices/UserProfileServiceTest.cs
+++ Ludus.Tests/DataServices/UserProfileServiceTest.cs
@@ -45,10 +45,10 @@ namespace Ludus.Tests.DataServices
         }
 
         /// <summary>
-        /// Ensure that a call to update actaully writes
+        /// Ensure that a biography actaully updates
         /// </summary>
         [TestMethod]
-        public void UpdateTest()
+        public void UpdateBiography()
         {
             // Arrange
             UserProfileService service = new UserProfileService();
@@ -88,6 +88,130 @@ namespace Ludus.Tests.DataServices
             // Assert
             Assert.IsNull(item);
         }
-        
+
+        /// <summary>
+        /// Ensure that a First Name actaully updates
+        /// </summary>
+        [TestMethod]
+        public void UpdateFirstName()
+        {
+            // Arrange
+            UserProfileService service = new UserProfileService();
+            // Act
+            var item = service.Find(1);
+            // Assert
+            Assert.IsNotNull(item);
+            // Act
+            var firstName = item.FirstName;
+            item.FirstName = String.Format("UPDATE FIRST NAME TEST");
+            var newFirstName = item.FirstName;
+            service.Update(item);
+            var item2 = service.Find(1);
+            // Assert
+            Assert.IsNotNull(item2);
+            // Assert
+            Assert.AreEqual(item2.FirstName, newFirstName);
+            item2.FirstName = firstName;
+            service.Update(item2);
+            var item3 = service.Find(1);
+            // Assert
+            Assert.IsNotNull(item2);
+            // Assert
+            Assert.AreEqual(item3.FirstName, firstName);
+        }
+
+        /// <summary>
+        /// Ensure that a Last Name actaully updates
+        /// </summary>
+        [TestMethod]
+        public void UpdateLastName()
+        {
+            // Arrange
+            UserProfileService service = new UserProfileService();
+            // Act
+            var item = service.Find(1);
+            // Assert
+            Assert.IsNotNull(item);
+            // Act
+            var lastName = item.LastName;
+            item.LastName = String.Format("UPDATE LAST NAME TEST");
+            var newLastName = item.LastName;
+            service.Update(item);
+            var item2 = service.Find(1);
+            // Assert
+            Assert.IsNotNull(item2);
+            // Assert
+            Assert.AreEqual(item2.LastName, newLastName);
+            item2.LastName = lastName;
+            service.Update(item2);
+            var item3 = service.Find(1);
+            // Assert
+            Assert.IsNotNull(item2);
+            // Assert
+            Assert.AreEqual(item3.LastName, lastName);
+        }
+
+        /// <summary>
+        /// Ensure that a Display Name actaully updates
+        /// </summary>
+        [TestMethod]
+        public void UpdateDisplayName()
+        {
+            // Arrange
+            UserProfileService service = new UserProfileService();
+            // Act
+            var item = service.Find(1);
+            // Assert
+            Assert.IsNotNull(item);
+            // Act
+            var displayName = item.DisplayName;
+            item.DisplayName = String.Format("UPDATE DISPLAY NAME TEST");
+            var newDisplayName = item.DisplayName;
+            service.Update(item);
+            var item2 = service.Find(1);
+            // Assert
+            Assert.IsNotNull(item2);
+            // Assert
+            Assert.AreEqual(item2.DisplayName, newDisplayName);
+            item2.DisplayName = displayName;
+            service.Update(item2);
+            var item3 = service.Find(1);
+            // Assert
+            Assert.IsNotNull(item2);
+            // Assert
+            Assert.AreEqual(item3.DisplayName, displayName);
+        }
+
+        /// <summary>
+        /// Ensure that a Email Address actaully updates
+        /// </summary>
+        [TestMethod]
+        public void UpdateEmailAddressName()
+        {
+            // Arrange
+            UserProfileService service = new UserProfileService();
+            // Act
+            var item = service.Find(1);
+            // Assert
+            Assert.IsNotNull(item);
+            // Act
+            var emailAddress = item.EmailAddress;
+            item.EmailAddress = String.Format("UPDATE EmailAddress TEST");
+            var newEmailAddress = item.EmailAddress;
+            service.Update(item);
+            var item2 = service.Find(1);
+            // Assert
+            Assert.IsNotNull(item2);
+            // Assert
+            Assert.AreEqual(item2.EmailAddress, newEmailAddress);
+            item2.EmailAddress = emailAddress;
+            service.Update(item2);
+            var item3 = service.Find(1);
+            // Assert
+            Assert.IsNotNull(item2);
+            // Assert
+            Assert.AreEqual(item3.EmailAddress, emailAddress);
+        }
+
     }
 }
